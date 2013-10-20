8c5c064286a75a4a1fdd018494d3c4169a58c436
oleg <oleg.korobenko@gmail.com>
2013-10-21 02:17:36 +0400

    start working on projections

--- test/collection.js
+++ test/collection.js
@@ -31,32 +31,38 @@ var tasks = [{
 	id: 1,
 	title: 'Task 1 at project 1',
 	project: 'project 1',
-	version: '1.0.0'
+	version: '1.0.0',
+	assignee: 'bob'
 }, {
 	id: 2,
 	title: 'Task 2 at project 1',
 	project: 'project 1',
-	version: '1.0.0'
+	version: '1.0.0',
+	assignee: 'jane'
 }, {
 	id: 3,
 	title: 'Task 1 at project 2',
 	project: 'project 2',
-	version: '2.0'
+	version: '2.0',
+	assignee: 'bob'
 }, {
 	id: 4,
 	title: 'Task 2 at project 2',
 	project: 'project 2',
-	version: '2.0'
+	version: '2.0',
+	assignee: 'jane'
 }, {
 	id: 5,
 	title: 'Task 1 at project 3',
 	project: 'project 3',
-	version: '0.1'
+	version: '0.1',
+	assignee: 'sam'
 }, {
 	id: 6,
 	title: 'Task 2 at project 3',
 	project: 'project 3',
-	version: '0.2'
+	version: '0.2',
+	assignee: 'sam'
 }];
 
 describe('simple batch (without projections)', function() {
@@ -98,6 +104,40 @@ describe('simple batch (without projections)', function() {
 			expect(err).have.property(
 				'message', 'Key not found in database [tasks]'
 			);
+			done();
+		});
+	});
+});
+
+describe('batch with projections', function() {
+	var tasksBatch = null;
+
+	it('created without errors', function(done) {
+		tasksBatch = new Batch(db, 'tasks', {
+			projections: [
+				['project', 'version', 'assignee', 'id'],
+				['assignee', 'project', 'version', 'id']
+			]
+		});
+		done();
+	});
+
+	it('put key and value without errors', function(done) {
+		var putCount = 0;
+		tasks.forEach(function(task) {
+			tasksBatch.put(task, function() {
+				putCount++;
+				if (putCount == tasks.length) done();
+			});
+		});
+	});
+
+	it('found value by start end', function(done) {
+		tasksBatch.find({
+			start: {project: 'project 2'}
+		}, function(err, data) {
+			if (err) {done(err); return;}
+			expect(data).eql(tasks.slice(2, 4));
 			done();
 		});
 	});
